a4b7a4793d4d816890701c8c2443a0e93c792eeb
Camilo Vega <camilovegag@unisabana.edu.co>
2022-10-08 08:07:36 -0500

    Move toggle logic to another component

--- ui/pages/Settings/SettingsEnableHoprRPCh.tsx
+++ ui/pages/Settings/SettingsEnableHoprRPCh.tsx
@@ -1,74 +1,17 @@
 import React, { ReactElement, useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
-import { useDispatch, useSelector } from "react-redux"
-import {
-  selectEnableHoprRPCh,
-  setNewEnableHoprValue,
-} from "@tallyho/tally-background/redux-slices/ui"
+import SharedButton from "../../components/Shared/SharedButton"
 import SharedPageHeader from "../../components/Shared/SharedPageHeader"
-import SharedToggleButton from "../../components/Shared/SharedToggleButton"
-
-function SettingRow(props: {
-  title: string
-  component: () => ReactElement
-}): ReactElement {
-  const { title, component } = props
-
-  return (
-    <li>
-      <div className="left">{title}</div>
-      <div className="right">{component()}</div>
-      <style jsx>
-        {`
-          li {
-            height: 50px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-          }
-          .left {
-            color: var(--green-20);
-            font-size: 18px;
-            font-weight: 600;
-            line-height: 24px;
-          }
-        `}
-      </style>
-    </li>
-  )
-}
+import SettingsEnableHoprToggle from "./SettingsEnableHoprToggle"
 
 export default function SettingsEnableHoprRPCh(): ReactElement {
   const { t } = useTranslation()
   const [, setSearchUri] = useState("")
   const uriInput = useRef<HTMLInputElement | null>(null)
-  const dispatch = useDispatch()
-
-  const toggleEnableHopr = (enableHoprValue: boolean) => {
-    dispatch(setNewEnableHoprValue(enableHoprValue))
-  }
-
-  const enableHoprRPCh = useSelector(selectEnableHoprRPCh)
 
   useEffect(() => {
     uriInput.current?.focus()
   }, [uriInput])
-
-  const enableHopr = {
-    title: t("settings.enableHopr"),
-    component: () => (
-      <SharedToggleButton
-        onChange={(toggleValue) => toggleEnableHopr(toggleValue)}
-        value={enableHoprRPCh}
-      />
-    ),
-  }
-
-  const generalList = [enableHopr]
-
-  const settings = {
-    general: generalList,
-  }
 
   return (
     <div className="standard_width_padded">
@@ -85,19 +28,22 @@ export default function SettingsEnableHoprRPCh(): ReactElement {
           placeholder={t("settings.enableHoprRPCh.uriTextfield")}
           onChange={(event) => setSearchUri(event.target.value)}
         />
+        <SharedButton
+          type="secondary"
+          size="medium"
+          onClick={() => {
+            window.open(`https://chat.tally.cash/`, "_blank")?.focus()
+          }}
+        >
+          {t("settings.enableHoprRPCh.uriBtn")}
+        </SharedButton>
       </section>
       <section>
         <h2>{t("settings.enableHoprRPCh.toggleTitle")}</h2>
         <p className="simple_text remove_bottom_margin">
           {t("settings.enableHoprRPCh.toggleDesc")}
         </p>
-        {settings.general.map((setting) => (
-          <SettingRow
-            key={setting.title}
-            title={setting.title}
-            component={setting.component}
-          />
-        ))}
+        <SettingsEnableHoprToggle />
       </section>
       <style jsx>{`
         h2 {
@@ -119,6 +65,7 @@ export default function SettingsEnableHoprRPCh(): ReactElement {
           padding-right: 56px;
           box-sizing: border-box;
           color: var(--green-40);
+          margin-bottom: 20px;
         }
         .uri_input::placeholder {
           color: var(--green-40);
